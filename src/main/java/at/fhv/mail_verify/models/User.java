24245add7b39d1cdbd7c18396213a9d4e06cf41c
Travis CI User <travis@example.org>
2018-02-07 11:58:18 +0000

    Update generated code

--- src/main/java/at/fhv/mail_verify/models/User.java
+++ src/main/java/at/fhv/mail_verify/models/User.java
@@ -15,6 +15,11 @@ public class User {
 	 */
 	private at.fhv.mail_verify.models.History history = null;
 	
+	/**
+	 * Description of the property name.
+	 */
+	public String name = null;
+	
 	// Start of user code (user defined attributes)
 	
 	// End of user code
@@ -33,6 +38,14 @@ public class User {
 		
 		public void setHistory(at.fhv.mail_verify.models.History history){
 		   this.history = history;
+		}
+		
+		public String getName(){
+		   return this.name;
+		}
+		
+		public void setName(String name){
+		   this.name = name;
 		}
 		
 	
